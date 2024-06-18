61ebc468164001c13444b33fdfc169be65fc0356
Tombeau Chrisnor <tombeauc@gmail.com>
2024-06-18 14:17:20 -0400

    chat message

--- src/com/socket/server/ServerMT.java
+++ src/com/socket/server/ServerMT.java
@@ -8,9 +8,12 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerMT extends Thread{
     private int nombreClient;
+    private List<Conversation> clients = new ArrayList<Conversation>();
 	public static void main(String[] args) {
 		new ServerMT().start();
 	}
@@ -23,7 +26,10 @@ public class ServerMT extends Thread{
 		 while(true) {
 			 Socket socket = ss.accept();
 			 ++nombreClient;
-			 new Conversation(socket, nombreClient).start();
+			 Conversation conversation = new Conversation(socket, nombreClient);
+			 clients.add(conversation);
+			 conversation.start();
+			 //new Conversation(socket, nombreClient).start();
 		 }
 		}
 		catch(IOException e) {
@@ -32,12 +38,31 @@ public class ServerMT extends Thread{
 	}
     
 	class Conversation extends Thread{
-		private Socket socket; 
-		private int numeroClient;
+		protected Socket socket; 
+		protected int numeroClient;
 		public Conversation(Socket socket, int numeroClient) {
 			this.socket = socket;
 			this.numeroClient=numeroClient;
 		}
+		
+		public void broadcastMessage(String message, Socket socket,int numeroClient) {
+			
+			try {
+				for(Conversation client : clients) {
+					if(client.socket!=socket)
+					{
+						if(client.numeroClient==numeroClient || numeroClient==-1) {
+							PrintWriter pw = new PrintWriter(client.socket.getOutputStream(),true);
+							 pw.println(message);
+						}
+					 
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		@Override
 		public void run() {
 			try {
@@ -52,8 +77,20 @@ public class ServerMT extends Thread{
 				pw.println("BienVenue, vous etes le client numero "+numeroClient);
 				while(true){
 					String req = br.readLine();
-					System.out.println("Le client "+IP+ " a envoye une requete "+req);
-					pw.println(req.length());
+					if(req.contains("=>")) {
+						String[] requestParams = req.split("=>");
+						if(requestParams.length==2) {
+							String message = requestParams[1];
+							int numeroClient=Integer.parseInt(requestParams[0]);
+							broadcastMessage(message, socket,numeroClient);
+						}
+						
+					}
+					else
+						broadcastMessage(req,socket,-1);
+					
+					//System.out.println("Le client "+IP+ " a envoye une requete "+req);
+					//pw.println(req.length());
 				}
 			}
 			catch(IOException e) {
